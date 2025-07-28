24c5296ba1e1a5d72b8f06a12dca9c72d0060c8b
hugoc82 <hugocapelle@orange.fr>
2025-07-28 09:52:06 +0200

    .

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -38,3 +38,21 @@ class SessionAuth(Auth):
         SessionAuth.user_id_by_session_id[session_id] = user_id
 
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """
+        Retrieve a user ID based on a given session ID.
+
+        Args:
+            session_id (str): The session ID.
+
+        Returns:
+            str: The user ID associated with the session ID,
+                 or None if not found or invalid.
+        """
+        if session_id is None:
+            return None
+        if not isinstance(session_id, str):
+            return None
+
+        return SessionAuth.user_id_by_session_id.get(session_id)
