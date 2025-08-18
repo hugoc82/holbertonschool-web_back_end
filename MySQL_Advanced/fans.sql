ccbf5e7a525ed03df94c9518bab51a3d47ecb9b4
hugoc82 <hugocapelle@orage.fr>
2025-08-18 14:42:36 +0000

    .

--- MySQL_Advanced/fans.sql
+++
@@ -1,9 +0,0 @@
--- 2. Fans
--- SQL script that creates the table fans
-
-CREATE TABLE IF NOT EXISTS fans (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255)
-);
-
